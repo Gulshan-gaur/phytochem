f6b99d28736858a94d895cc05bd26ca03b2540df
Gulshan-gaur <gulssh3@gmail.com>
2023-04-13 16:06:42 +0530

    update version

--- controllers/searchcontrol.js
+++ controllers/searchcontrol.js
@@ -13,24 +13,76 @@ const get_search = function(req,res){
 };
 
 
-const idResult = function(req,res){
-      const smile=req.query.smile;
+// const idResult = function(req,res){
+      // const smile=req.query.smile;
 
-      Phytochemical.find({SMILES:smile},function(err,found,next){
-        if(err){
-          logger.log("error occured");
-          return next(err);
-        }
-        if(found.length){
-          res.render("result",{empty:null,idnn:found});
-          logger.log("info", "data retrieved");
-        }
-        else {
-          res.render("result",{empty : "No data Found", idnn:[]});
-          logger.log("warn", "no data found");
-        }
-    });
+      // Phytochemical.find({SMILES:smile},function(err,found,next){
+        // if(err){
+          // logger.log("error occured");
+          // return next(err);
+        // }
+        // if(found.length){
+          // res.render("result",{empty:null,idnn:found});
+          // logger.log("info", "data retrieved");
+        // }
+        // else {
+          // res.render("result",{empty : "No data Found", idnn:[]});
+          // logger.log("warn", "no data found");
+        // }
+    // });
     // console.log(Phytochemical.find({SMILES:smile}))
+// };
+const idResult = function(req, res) {
+  const smile = req.query.smile;
+  const pubchem_cid = req.query.pubchem_cid;
+  const mol_formula = req.query.mol_formula;
+  if (smile) {
+    Phytochemical.find({ SMILES: smile }, function(err, found, next) {
+      if (err) {
+        logger.log('error occurred');
+        return next(err);
+      }
+      if (found.length) {
+        res.render('result', { empty: null, idnn: found });
+        logger.log('info', 'data retrieved');
+      } else {
+        res.render('result', { empty: 'No data Found', idnn: [] });
+        logger.log('warn', 'no data found');
+      }
+    });
+  } else if (pubchem_cid) {
+    Phytochemical.find({ PubChem_CID: pubchem_cid }, function(err, found, next) {
+      if (err) {
+        logger.log('error occurred');
+        return next(err);
+      }
+      if (found.length) {
+        res.render('result', { empty: null, idnn: found });
+        logger.log('info', 'data retrieved');
+      } else {
+        res.render('result', { empty: 'No data Found', idnn: [] });
+        logger.log('warn', 'no data found');
+      }
+    });
+  } else if (mol_formula) {
+    Phytochemical.find({ Molecular_formula: mol_formula }, function(err, found, next) {
+      if (err) {
+        logger.log('error occurred');
+        return next(err);
+      }
+      if (found.length) {
+        found = found.slice(0, 3);
+        res.render('result', { empty: null, idnn: found});
+        logger.log('info', 'data retrieved');
+      } else {
+        res.render('result', { empty: 'No data Found', idnn: [] });
+        logger.log('warn', 'no data found');
+      }
+    });
+  } else {
+    res.render('result', { empty: 'Invalid query type', idnn: [] });
+    logger.log('warn', 'invalid query type');
+  }
 };
 
 // const keywordResult = function(req,res){
@@ -78,48 +130,48 @@ const molgreater =  async function (req,res){
     const h_don = req.query.h_donor;
     const h_acc = req.query.h_acceptor;
     const ring =req.query.aro_ring
-  var {page =1, limit = 10}=req.query;
-  const data = await Phytochemical.find({
-    Molecular_weight: { $gt: molwig, $lt: less},
-    Aromatic_rings: ring,
-    H_bond_donors: h_don,
-    H_bond_acceptors: h_acc
-  }).limit(limit*1).skip((page-1)*limit).exec();
-  var count = await Phytochemical.find({
-    Molecular_weight: { $gt: molwig, $lt: less},Aromatic_rings: ring,H_bond_donors: h_don,H_bond_acceptors: h_acc
-  }).countDocuments();
-  if(count >=200){
-
-  count = 200;
-  }
-  if(data.length){
-
-      res.render("resultgt",{
-        empty:null, molwg: data, totalPages: Math.ceil(count/limit),page: page,
-        wg:molwig,less:less,h_donor:h_don,h_acceptor:h_acc,aro_ring:ring});
-      logger.log("info","data retrieved");
-      }
-      else {
+    var {page =1, limit = 10}=req.query;
+    const data = await Phytochemical.find({
+      Molecular_weight: { $gt: parseInt(molwig), $lt: parseInt(less) },
+      Aromatic_rings: parseInt(ring),
+      H_bond_donors: parseInt(h_don),
+      H_bond_acceptors: parseInt(h_acc)
+    }).limit(limit * 1).skip((page - 1) * limit).exec();
+    console.log(data)
+    var count = await Phytochemical.find({
+      Molecular_weight: { $gt: parseInt(molwig), $lt: parseInt(less) },
+      Aromatic_rings: parseInt(ring),
+      H_bond_donors: parseInt(h_don),
+      H_bond_acceptors: parseInt(h_acc)
+    }).countDocuments();
+    if(count >=200){
+  
+    count = 200;
+    }
+    if(data.length){
         res.render("resultgt",{
-          empty : "No data Found", molwg:[],totalPages: Math.ceil(count/limit),page: page,
+          empty:null, molwg: data, totalPages: Math.ceil(count/limit),page: page,
           wg:molwig,less:less,h_donor:h_don,h_acceptor:h_acc,aro_ring:ring});
-        logger.log("warn", "no data found");
-      }
-}
-
-catch(err){
+        logger.log("info","data retrieved");
+        }
+        else {
+          res.render("resultgt",{
+            empty : "No data Found", molwg:[],totalPages: Math.ceil(count/limit),page: page,
+            wg:molwig,less:less,h_donor:h_don,h_acceptor:h_acc,aro_ring:ring});
+          logger.log("warn", "no data found");
+        }
+  }  
+  catch(err){
   if(err){
     console.log("error occured");
-    console.log(err);
+    console.log(err)
   }
-}
+  }
 };
 
 
 module.exports = {
-    get_search,
-    idResult,
-    // structureResult,
-    // keywordResult,
-    molgreater,
+  get_search,
+  idResult,
+  molgreater
 };
